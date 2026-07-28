fdb101c72c2664c274aa17b07c2234a3c2e5dd32
2436744839@qq.com <2436744839@qq.com>
2026-07-28 14:09:38 +0800

    Merge worktree branch fix-login-token-401-error-AYiLIu

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -1,15 +1,20 @@
 package com.hmdp.controller;
 
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
 
 /**
  * <p>
@@ -25,10 +30,19 @@ public class ShopTypeController {
     @Resource
     private IShopTypeService typeService;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("list")
     public Result queryTypeList() {
+        String json = stringRedisTemplate.opsForValue().get(CACHE_SHOP_TYPE_KEY);
+        if (StrUtil.isNotBlank(json)) {
+            List<ShopType> typeList = JSONUtil.toList(json, ShopType.class);
+            return Result.ok(typeList);
+        }
         List<ShopType> typeList = typeService
                 .query().orderByAsc("sort").list();
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_TYPE_KEY, JSONUtil.toJsonStr(typeList));
         return Result.ok(typeList);
     }
 }
